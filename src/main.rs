7d04d0ce42b10eaf1526714ddadce92d9cf9704e
rachit <rachit.bhi004@gmail.com>
2024-07-28 18:05:13 +0530

    add prev year and next year buttons

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-
 //use iced::theme::{Custom, Palette};
+
 use iced::{executor, Color, Point};
 use iced::{Element, Application,window, Settings,Theme,Command,Size};
-use iced::widget::{Button, Container, Row,Column, Text};
+use iced::widget::{Button, Column, Container, Row, Text};
 use chrono::{Datelike, Local, NaiveDate};
 use iced::window::{Position::Specific,Level,settings::PlatformSpecific};
-use iced::keyboard;
-use iced::Subscription;
-use iced::window;
+
 
 #[derive(Default)]
 struct CalendarApp {
@@ -21,20 +19,22 @@ struct CalendarApp {
 enum Message {
     PrevMonth,
     NextMonth,
-    DateSelected(NaiveDate), 
-    ToggleFullscreen(window::Mode),
+    PrevYear,
+    NextYear,
+    DateSelected(NaiveDate),
 }
 
 
 
 fn main() -> iced::Result {
-    let height = 250.0;
-    let width = 400.0;
+    let scale = 1.0;
+    let height = 250.0*scale;
+    let width = 400.0*scale;
+    
     let x =960.0;
     let y = 480.0;
     
-    
-    let settings = Settings {
+    let settings = Settings {           
         window: window::Settings {
             size: (Size::new(width,height)),
             exit_on_close_request: false,
@@ -80,10 +80,6 @@ impl Application for CalendarApp {
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::ToggleFullscreen(mode) => {  
-                window::change_mode(window, mode);  
-                return Command::none();
-            }
             Message::PrevMonth => {
                 let (year, month) = (self.selected_date.year(), self.selected_date.month());
                 let new_month = if month == 1 { 12 } else { month - 1 };
@@ -96,10 +92,22 @@ impl Application for CalendarApp {
                 let new_year = if month == 12 { year + 1 } else { year };
                 self.selected_date = NaiveDate::from_ymd_opt(new_year, new_month, 1).expect("err");
             }
+            Message::PrevYear => {
+                let year = self.selected_date.year()-1;
+                let mut month = 1;
+                if year == self.today.year() { month = self.today.month();} 
+                self.selected_date = NaiveDate::from_ymd_opt(year, month, 1).expect("err");
+            }
+            Message::NextYear => {
+                let year = self.selected_date.year()+1;
+                let mut month = 1;
+                if year == self.today.year() { month = self.today.month();} 
+                self.selected_date = NaiveDate::from_ymd_opt(year, month, 1).expect("err");
+            }
             Message::DateSelected(date) => {
                 self.selected_date = date;    
                 println!("date:{:?}",date);
-
+                
             }
             
         }
@@ -111,25 +119,6 @@ impl Application for CalendarApp {
        // custom_theme::NordTheme::default()
     }
 
-    fn subscription(&self) -> Subscription<Message> {
-        use keyboard::key;
-
-        keyboard::on_key_press(|key, modifiers| {
-            let keyboard::Key::Named(key) = key else {
-                return None;
-            };
-
-            match (key, modifiers) {
-                (key::Named::ArrowUp, keyboard::Modifiers::SHIFT) => {
-                    Some(Message::ToggleFullscreen(window::Mode::Fullscreen))
-                }
-                (key::Named::ArrowDown, keyboard::Modifiers::SHIFT) => {
-                    Some(Message::ToggleFullscreen(window::Mode::Windowed)) 
-                }
-                _ => None,
-            }
-        })
-    }
 
     fn view(&self) -> Element<Message> {
         let month = self.selected_date.format("%B %Y").to_string();
@@ -142,7 +131,6 @@ impl Application for CalendarApp {
             .push(
                 Row::new().align_items(iced::Alignment::Center)
                     .push(Button::new( Text::new("<")).on_press(Message::PrevMonth))
-                    
                     .push(Text::new(month)
                     .horizontal_alignment(iced::alignment::Horizontal::Center))
                     
@@ -150,9 +138,10 @@ impl Application for CalendarApp {
                     .push(Button::new( Text::new(">")).on_press(Message::NextMonth)),
             )
             .push(Row::with_children(days.iter().map(|&day| Text::new(day)
-            .width(button_size+10)
-            .horizontal_alignment(iced::alignment::Horizontal::Center).into())));
+                .width(button_size+10)
+                .horizontal_alignment(iced::alignment::Horizontal::Center).into())));
         
+            
         
 
       
@@ -163,7 +152,7 @@ impl Application for CalendarApp {
         
 
         for _week in 0..6 {
-            let mut week_row = Row::new();
+            let mut week_row = Row::new().align_items(iced::Alignment::End);
              
             for _day in 0..7 {
                 
@@ -181,6 +170,7 @@ impl Application for CalendarApp {
                                         month_button_col[1], 
                                         month_button_col[2]))))
                                     .on_press(Message::DateSelected(date));
+                                    
                 week_row = week_row.push(date_label);
                 date = date + chrono::Duration::days(1);
             }
@@ -188,12 +178,20 @@ impl Application for CalendarApp {
         }
         let mut calender_row = Row::new().push(calender_frame);
         
+        let mut big_week_col = Column::new();
+        let year = self.selected_date.format("%Y").to_string();
+        let year_frame = Column::new()
+            .push(
+                Row::new().align_items(iced::Alignment::Center)
+                    .push(Button::new( Text::new("<")).on_press(Message::PrevYear))
+                    .push(Text::new(year)
+                    .horizontal_alignment(iced::alignment::Horizontal::Center))
+                    
+                    
+                    .push(Button::new( Text::new(">")).on_press(Message::NextYear)),
+            );
+        big_week_col = big_week_col.push(year_frame);
         
-
-        let mut big_week_row = Column::new().align_items(iced::Alignment::End);
-        
-        
-
         date = self.selected_date - chrono::Duration::days((self.selected_date.weekday().number_from_monday() - 1) as i64);
         println!("Monday :{:?}",date);
     
@@ -209,19 +207,20 @@ impl Application for CalendarApp {
             
             let week_label = Button::new( Text::new(week_date)
                             .horizontal_alignment(iced::alignment::Horizontal::Center)
+                            //.vertical_alignment(iced::alignment::Vertical::Center)
                             .width(button_size*5) 
                             .style(iced::theme::Text::Color(Color::from_rgb(week_button_col[0],
                                                                             week_button_col[1], 
                                                                             week_button_col[2]))))
                             .on_press(Message::DateSelected(date));
             
-            big_week_row = big_week_row.push(week_label);
-            
-             
+            big_week_col = big_week_col.push(week_label);
             date = date + chrono::Duration::days(1);
         }
         
-        calender_row = calender_row.push(big_week_row);
+        calender_row = calender_row.push(big_week_col);
+        
+        
         main_content_frame = main_content_frame.push(calender_row);  
         
         Container::new(main_content_frame).center_x().center_y().into()
